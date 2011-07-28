9f6ed8722c721cd8e8161b5ff48511e2b2475d20
Benxamin Porto Dominguez <benxamin.porto@gmail.com>
2011-07-28 21:24:57 +0200

    ProyectoEclippseConScala

---
+++ DemostradorTecnologico/src/org/tech/galician/ClaseEscalaTest.scala
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.tech.galician
+
+import scala.runtime.ObjectRef
+
+/**
+ * Esta clase es un  Object que tiene un ciclo de vida Singleton
+ * Pensando en un servicio o una función. 
+ * 
+ * @author Benxa
+ *
+ */
+object ClaseEscalaTest extends ObjectRef {
+
+  def main(args: Array[String]): Unit = {
+    
+    println("Probando Escala")
+  }
+
+}
